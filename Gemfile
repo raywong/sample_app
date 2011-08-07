e7f5fbdc219b2ea982241d454d91fdd380161dd0
Ray Wong <wongyuanrui@gmail.com>
2011-08-07 18:36:14 -0400

    Done with sign in

--- Gemfile
+++ Gemfile
@@ -9,12 +9,14 @@ gem 'sqlite3','1.3.3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
-  gem 'rspec-rails', '2.6.1'
+  gem 'rspec-rails', '2.0.0.beta.18'
+  #gem 'rspec-rails', '2.6.1'
   gem 'annotate', '2.4.0'
 end
 
 group :test do
-  gem 'rspec-rails', '2.6.1'
+  gem 'rspec-rails', '2.0.0.beta.18'
+  #gem 'rspec-rails', '2.6.1'
   gem 'webrat', '0.7.1'
   gem 'autotest', '4.4.6'
   gem 'autotest-rails-pure', '4.1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
  
+ 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
  # get "users/new"
  # get "pages/home"
@@ -9,10 +11,14 @@ SampleApp::Application.routes.draw do
 
 
   match '/signup',  :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  match '/signup',    :to => 'users#new'
+
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  
   root :to=> 'pages#home'
 
   # The priority is based upon order of creation:
